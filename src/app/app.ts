4a28e573267125169ba158d01963e7ef34d8d157
ingcretara <cretaradev@gmail.com>
2026-08-29 10:32:15 +0200

    update app structure and integrate Angular Material components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { MatNavList } from '@angular/material/list';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { map } from 'rxjs';
+import { NgClass } from '@angular/common';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavContent,
+    MatSidenavContainer,
+    MatSidenav,
+    MatNavList,
+    MatCard,
+    MatCardHeader,
+    MatCardSubtitle,
+    MatCardContent,
+    MatCardTitle,
+    NgClass,
+  ],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('angular-22-new-features');
+  private readonly breakpointObserver = inject(BreakpointObserver);
+
+  mobileQuery = toSignal(
+    this.breakpointObserver.observe('(max-width: 600px)').pipe(map(({ matches }) => matches)),
+    { initialValue: false },
+  );
+  header = 'Angular 22 new features';
+
+  constructor() {}
 }
